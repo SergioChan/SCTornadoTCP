cb82437e7798d8456a53007babd1442f78d3fa52
chenYuheng <yuheng@tataufo.com>
2015-07-11 14:13:32 +0800

    Init Request

--- Server/Request/Request.py
+++ Server/Request/Request.py
@@ -1,1 +1,52 @@
 __author__ = 'Yuheng Chen'
+import demjson
+import time
+
+class Request(object):
+    '''
+    `Request` is the basic object type in the stream.
+    Each `Request` is delimited by a delimiter '\n'.
+    `Request` transported in a serialization mode, using normal json type.
+
+    To extend `Request`, define a subclass and there is no need to override anything.
+
+    `Request` has following parameters:
+
+    1.`address`:simple ip address combined with port number representing
+                the request origin server.
+
+    2.`rawBody`:raw content of the request body
+
+    3.`cmdid`:command id defines in Command
+
+    4.`timestamp`:the date when request was sent
+
+    5.`params`:a dict that storage all the data it takes
+    '''
+    def __init__(self, address = None, Body = None):
+        self.address = address
+
+        if not Body:
+            self.rawBody = '{}'
+        else:
+            self.rawBody = Body
+
+        data = demjson.decode(self.rawBody)
+        self.cmdid = None
+        self.timestamp = int(time.time())
+        self.params = dict()
+
+        if data.has_key('cmdid'):
+            self.cmdid = data['cmdid']
+        if data.has_key('timestamp'):
+            self.timestamp = data['timestamp']
+        if data.has_key('params'):
+            self.params = data['params']
+
+    def serialization(self):
+        '''
+        used for serialization storage
+        :return: a string value represents the reqeust
+        '''
+        tmp = {'address':self.address,'body':self.rawBody}
+        return demjson.encode(tmp)
